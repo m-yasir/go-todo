d0206c71a6b8b9ea2998eb2674121e9d5bc4e232
Yasir Nadeem <m-yasir.dev@protonmail.com>
2023-03-09 03:39:35 +0500

    feat(setup): setup todo routes and db

--- pkg/setup/setup.go
+++ pkg/setup/setup.go
@@ -1,11 +1,34 @@
 package setup
 
 import (
+	"github.com/m-yasir/go-todo/pkg/db"
 	routes "github.com/m-yasir/go-todo/pkg/routes"
 
 	"github.com/labstack/echo/v4"
 )
 
+type RouteGroup struct {
+	name    string
+	handler func(*echo.Group)
+}
+
+func SetupGroupRoutes(app *echo.Echo, routeGroups []RouteGroup) {
+	for _, routeGroup := range routeGroups {
+		g := app.Group(routeGroup.name)
+		routeGroup.handler(g)
+	}
+}
+
 func SetupRoutes(app *echo.Echo) {
+	// Setup individual routes
 	app.GET("/ping", routes.HandlePing)
+
+	// Setup grouped routes
+	SetupGroupRoutes(app, []RouteGroup{
+		{"/todos", routes.HandleTodoRoutes},
+	})
+}
+
+func SetupDb() {
+	db.SeedTodosDb()
 }
